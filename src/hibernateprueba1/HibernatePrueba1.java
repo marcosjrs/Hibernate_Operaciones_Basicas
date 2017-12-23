f620c7b5ad3c28362976c474c8d3b72bdb73ac1b
marcosjrs <marcosjrs80@gmail.com>
2017-12-23 17:20:17 +0100

    Probamos los métodos insertar y buscarPorId

--- src/hibernateprueba1/HibernatePrueba1.java
+++ src/hibernateprueba1/HibernatePrueba1.java
@@ -13,11 +13,24 @@ public class HibernatePrueba1 {
     public static void main(String[] args) {
        
         PersonaDao personaDao = new PersonaDao();
+        
+        //Creamos una instancia de Persona y la insertamos
+        Persona Miriam = new Persona();
+        Miriam.setNombre("Miriam");
+        Miriam.setApellido("VB");
+        personaDao.insertar(Miriam);   
+        
+        //Listamos todas las personas, recordando que implementa toString()
         List<Persona> personas = personaDao.listar();
-        //imprimos los datos de persona, recordando que implementa toString()
         for(Persona p: personas){
             System.out.println("persona:" + p);
         }
+        
+        //Buscamos por un id
+        Persona personaABuscar = new Persona();
+        personaABuscar.setIdPersona(personas.get(0).getIdPersona());
+        Persona personaEncontrada = personaDao.buscarPorId(personaABuscar );
+        System.out.println("persona encontrada:" + personaEncontrada);
     }
 
 }
