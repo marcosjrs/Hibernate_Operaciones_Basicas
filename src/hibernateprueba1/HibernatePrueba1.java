90076840d59e2ab0a7f48a99885adc1f6fa30269
marcosjrs <marcosjrs80@gmail.com>
2017-12-23 16:16:40 +0100

    Hacemos una consulta simple con la configuración (y con la entidad) creada

--- src/hibernateprueba1/HibernatePrueba1.java
+++ src/hibernateprueba1/HibernatePrueba1.java
@@ -1,12 +1,25 @@
 package hibernateprueba1;
 
+import java.util.List;
+import javax.persistence.*;
+import model.Persona;
+
 public class HibernatePrueba1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        //Comprobamos que todo funcione correctamente, haciendo una consulta
+        //Recordamos que tenemos configurado con nombre HibernateEjemplo1 en el persistence.xml
+        EntityManagerFactory factoria = Persistence.createEntityManagerFactory("HibernateEjemplo1");
+        EntityManager em = factoria.createEntityManager();
+        Query q = em.createQuery("SELECT p FROM Persona p");
+        List<Persona> personas = q.getResultList();
+        //imprimos los datos de persona, recordando que implementa toString()
+        for(Persona p: personas){
+            System.out.println("persona:" + p);
+        }
     }
 
 }
