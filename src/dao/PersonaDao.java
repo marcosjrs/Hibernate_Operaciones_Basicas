428aba1b9ab27d374e4af00972d93b879be1c5c9
marcosjrs <marcosjrs80@gmail.com>
2017-12-23 17:07:10 +0100

    Implementación de insertar (crear) una persona

--- src/dao/PersonaDao.java
+++ src/dao/PersonaDao.java
@@ -16,21 +16,35 @@ public class PersonaDao {
         // cogemos la configuracion "HibernateEjemplo1" del persistence.xml
         emf = Persistence.createEntityManagerFactory("HibernateEjemplo1");
     }
-    
+
     private EntityManager getEntityManager() {
         return emf.createEntityManager();
+    }
+
+    public void insertar(Persona persona) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();// Iniciamos una transaccion
+            em.persist(persona);// Insertamos la nueva persona en el entityManager
+            em.getTransaction().commit();// Hacemos efectivas las transacciones
+        } catch (Exception ex) {
+            System.out.println("Error al insetar objeto:" + ex.getMessage());
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     public List<Persona> listar() {
         em = getEntityManager();
         Query query = em.createQuery("SELECT p FROM Persona p"); //creamos con hql
         return query.getResultList();
-    } 
-    
+    }
+
     public Persona buscarPorId(Persona personaBuscarConId) {
         em = getEntityManager();
         return em.find(Persona.class, personaBuscarConId.getIdPersona());
     }
 
-    
 }
