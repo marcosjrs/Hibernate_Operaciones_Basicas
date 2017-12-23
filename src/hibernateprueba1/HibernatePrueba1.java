d3be5df475e062134eaad7f4e905c0e6d65c12d0
marcosjrs <marcosjrs80@gmail.com>
2017-12-23 15:50:19 +0100

    Creación Proyecto tipo java.

---
+++ src/hibernateprueba1/HibernatePrueba1.java
@@ -0,0 +1,12 @@
+package hibernateprueba1;
+
+public class HibernatePrueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
